02ab7c62a31c6e13ae965b5a256602377a39ddfb
Egor Bryzgalov <bryzgalov.e@yahoo.com>
2022-11-11 16:09:22 -0500

    Initial Commit

---
+++ scorecounter/scorecounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  scorecounterApp.swift
+//  scorecounter
+//
+//  Created by Egor Bryzgalov on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct scorecounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
